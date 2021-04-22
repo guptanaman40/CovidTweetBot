8fa27161989178e4ed070a2020b7ea3ffec963b4
Rahul Raina <rahulraina711@gmail.com>
2021-04-22 22:47:55 +0530

    added new keys in secret file, config.js in now avaialable

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SC,
+    access_token: process.env.ACC_TKN,
+    access_token_secret: process.env.ACC_TKN_SC,
+}

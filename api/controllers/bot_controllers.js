e5988c6233ac071a2c872a323b3c9503a7ece472
Rahul Raina <rahulraina711@gmail.com>
2021-04-23 18:11:56 +0530

    added regex

--- api/controllers/bot_controllers.js
+++ api/controllers/bot_controllers.js
@@ -1,26 +1,7 @@
 let Twit = require('twit');
 let Twitter = new Twit(require('../../config'));
 
-let db = [
-
-    "oxygen",
-    "plasma",
-    "bed",
-    "remdesivir",
-    "delhi",
-    "mumbai",
-    "pune",
-    "gandhinagar",
-    "ncr",
-    "maharashtra",
-    "gujarat",
-    "urgent",
-    "emergency",
-    "urgently",
-    "requirement",
-    "required"
-
-]
+let db = ['remdesivir','bed','plasma','oxygen','maharashtra', 'delhi', 'karnataka', 'telangana', 'gujarat', 'tamil nadu', 'west bengal', 'gujarat', 'maharashtra', 'rajasthan', 'uttar pradesh', 'uttar pradesh', 'maharashtra', 'madhya pradesh', 'maharashtra', 'madhya pradesh', 'andhra pradesh', 'maharashtra', 'bihar', 'gujarat', 'uttar pradesh', 'punjab', 'uttar pradesh', 'maharashtra', 'jharkhand', 'haryana', 'uttar pradesh', 'gujarat', 'maharashtra', 'maharashtra', 'uttar pradesh', 'jammu and kashmir', 'maharashtra', 'jharkhand', 'punjab', 'maharashtra', 'uttar pradesh', '[5]', 'west bengal', 'madhya pradesh', 'madhya pradesh', 'tamil nadu', 'andhra pradesh', 'rajasthan', 'tamil nadu', 'chhattisgarh', 'rajasthan', 'chandigarh', 'assam', 'maharashtra', 'karnataka', 'karnataka', 'tamil nadu', 'uttar pradesh', 'uttar pradesh', 'tamil nadu', 'haryana', 'uttar pradesh', 'punjab', 'odisha', 'tamil nadu', 'telangana', 'maharashtra', 'maharashtra', 'andhra pradesh', 'kerala', 'maharashtra', 'uttar pradesh', 'uttar pradesh', 'rajasthan', 'maharashtra', 'uttar pradesh', 'jharkhand', 'chhattisgarh', 'odisha', 'uttar pradesh', 'kerala', 'andhra pradesh', 'gujarat', 'uttarakhand', 'west bengal', 'west bengal', 'odisha', 'maharashtra', 'maharashtra', 'rajasthan', 'maharashtra', 'karnataka', 'gujarat', 'madhya pradesh', 'uttar pradesh', 'west bengal', 'uttar pradesh', 'maharashtra', 'jammu and kashmir', 'maharashtra', 'karnataka', 'tamil nadu', 'karnataka', 'andhra pradesh', 'tamil nadu', 'andhra pradesh', 'tamil nadu', 'maharashtra', 'bihar', 'andhra pradesh', 'rajasthan', 'andhra pradesh', 'karnataka', 'kerala', 'west bengal', 'west bengal', 'jharkhand', 'west bengal', 'karnataka', 'punjab', 'tripura', 'bihar', 'uttar pradesh', 'west bengal', 'west bengal', 'maharashtra', 'maharashtra', 'haryana', 'madhya pradesh', 'chhattisgarh', 'rajasthan', 'odisha', 'bihar', 'maharashtra', 'uttar pradesh', 'kerala', 'tamil nadu', 'andhra pradesh', 'andhra pradesh', 'west bengal', 'odisha', 'chhattisgarh', 'uttar pradesh', 'maharashtra', 'karnataka', 'uttar pradesh', 'karnataka', 'maharashtra', 'gujarat', 'kerala', 'rajasthan', 'west bengal', 'west bengal', 'telangana', 'maharashtra', 'karnataka', 'telangana', 'puducherry', 'bihar', 'haryana', 'bihar', 'west bengal', 'mizoram', 'madhya pradesh', 'maharashtra', 'haryana', 'punjab', 'maharashtra', 'andhra pradesh', 'west bengal', 'delhi', 'bihar', 'madhya pradesh', 'uttar pradesh', 'haryana', 'uttar pradesh', 'chhattisgarh', 'manipur', 'madhya pradesh', 'uttar pradesh', 'bihar', 'andhra pradesh', 'telangana', 'uttar pradesh', 'maharashtra', 'west bengal', 'rajasthan', 'bihar', 'delhi', 'gujarat', 'west bengal', 'tamil nadu', 'puducherry', 'rajasthan', 'tamil nadu', 'madhya pradesh', 'uttar pradesh', 'karnataka', 'rajasthan', 'telangana', 'assam', 'uttarakhand', 'andhra pradesh', 'andhra pradesh', 'tamil nadu', 'rajasthan', 'delhi', 'jharkhand', 'tamil nadu', 'uttar pradesh', 'west bengal', 'madhya pradesh', 'uttar pradesh', 'madhya pradesh', 'gujarat', 'telangana', 'west bengal', 'haryana', 'west bengal', 'tamil nadu', 'karnataka', 'bihar', 'haryana', 'madhya pradesh', 'odisha', 'west bengal', 'tamil nadu', 'gujarat', 'karnataka', 'delhi', 'west bengal', 'andhra pradesh', 'jharkhand', 'bihar', 'west bengal', 'madhya pradesh', 'andhra pradesh', 'madhya pradesh', 'uttar pradesh', 'gujarat', 'madhya pradesh', 'delhi', 'west bengal', 'haryana', 'west bengal', 'haryana', 'gujarat', 'uttar pradesh', 'uttar pradesh', 'uttar pradesh', 'andhra pradesh', 'maharashtra', 'uttar pradesh', 'uttar pradesh', 'jharkhand', 'tamil nadu', 'gujarat', 'west bengal', 'haryana', 'bihar', 'west bengal', 'delhi', 'madhya pradesh', 'uttar pradesh', 'maharashtra', 'madhya pradesh', 'gujarat', 'uttar pradesh', 'west bengal', 'kerala', 'kerala', 'andhra pradesh', 'himachal pradesh', 'andhra pradesh', 'karnataka', 'bihar', 'andhra pradesh', 'telangana', 'bihar', 'assam', 'assam', 'jharkhand', 'andhra pradesh', 'assam', 'bihar', 'bihar', 'andaman and nicobar islands ut', 'jharkhand', 'andhra pradesh', 'tamil nadu', 'assam', 'bihar', 'andhra pradesh', 'bihar', 'bihar', 'jharkhand', 'assam', 'andhra pradesh', 'andhra pradesh', 'bihar', 'andhra pradesh', 'jharkhand', 'andhra pradesh', 'punjab', 'tamil nadu', 'tamil nadu', 'andhra pradesh', 'telangana', 'telangana', 'andhra pradesh', 'tamil nadu', 'bihar', 'bihar', 'uttar pradesh', 'andhra pradesh', 'andhra pradesh', 'andhra pradesh', 'bihar', 'assam', 'bihar', 'bihar', 'sikkim', 'goa']
 
 exports.search_all_related_tweets = (req, res)=>{
     const {key} = req.query;
@@ -48,12 +29,35 @@ exports.search_all_related_tweets = (req, res)=>{
 exports.search_keywords_in_tweets = (req, res)=>{
 
     const {text} = req.body;
-    
-    let eachKeyword = text.split(" ");
-        let newArr = eachKeyword.filter(word => {
-            return db.includes(word)
-        });
-        return res.status(200).json({keywords: newArr});
+
+    // convert all letter to lowercase to match the local keywords
+    var string = text,
+    regex = /[A-Z]/g;
+
+    var modified = string.replace(regex, function(match) {
+      return match.toLowerCase();
+    });
+
+    // spliting alll the words to compare
+    let eachKeyword = modified.split(" ");
+    console.log(eachKeyword);
+
+    // get the keywords from local db
+    let newArr = eachKeyword.filter(word => {
+        return db.includes(word)
+    });
+
+    // convert first letter to Upper Case to match the keywords in Atlas DB
+    let responseArr = newArr.map(word=> {
+        let warray = word.split('');
+        warray[0] = warray[0].toUpperCase();
+        let newWord = '';
+        warray.map(letter=> newWord+=letter);
+        return newWord;
+    })
+
+    //respond with the matched keywords
+    return res.status(200).json({keywords: responseArr});
 };
 
 exports.retweet = (req, res)=>{
--- app.js
+++ app.js
@@ -3,11 +3,62 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const { urlencoded } = require('body-parser');
+const mongoose = require('mongoose');
 
 
 dotenv.config();
 
 const app = express();
+
+//mongodb+srv://covid_help_dev.omc18.mongodb.net/
+//:RA0U4RNVOQUCityE
+
+// mongoose.connect("mongodb+srv://covid_help_dev:RA0U4RNVOQUCityE@cluster0.omc18.mongodb.net/covid_help_dev?retryWrites=true&w=majority",err=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log("connected to mongo");
+//     const Suppliers = mongoose.model('Suppliers', 
+//                new mongoose.Schema({ url: String, text: String, id: Number}), 
+//                'Suppliers'); 
+
+//     Suppliers.find().exec().then(
+//         doc=>console.log(doc)
+//         .catch(err=> console.log(err))
+//     );  
+
+// })
+
+// const collection = Object.keys(mongoose.connection.collections);
+
+// console.log(collection);
+
+
+// connection.on('open', function () {
+//     connection.db.listCollections().toArray(function (err, names) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log(names);
+//       }
+
+//       mongoose.connection.close();
+//     });
+// });
+
+// const connectMongo = (mongoURI) => {
+//     mongoose.connect(mongoURI, { useNewUrlParser: true , useUnifiedTopology: true });
+//     mongoose.connection.on('open', function(){
+//         console.log(mongoose.connection.db.suppliers.findOne())
+//     })
+//     mongoose.connection.on('error', (err) => {
+//      if (err) console.log(err);
+
+//     });
+//    }
+
+// connectMongo("mongodb+srv://covid_help_dev:RA0U4RNVOQUCityE@cluster0.omc18.mongodb.net/covid_help_dev?retryWrites=true&w=majority");
+             
 
 app.use(cors());
 app.use(morgan('dev'));
